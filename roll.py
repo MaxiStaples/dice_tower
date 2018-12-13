03bf40a2fe4450baca7850ee0366f28275c1c2eb
tacurry3 <tacurry3@asu.edu>
2018-12-13 14:12:36 -0800

    Added modifiers and cleaned up print

--- roll.py
+++ roll.py
@@ -2,11 +2,11 @@ import random
 
 #roll_in = [iterations, num_dice, game_id, sides_dice, roll_mod, kd_mod, kd_num, rl_mod, rl_num]
 
-iterations = 6
+iterations = 5
 num_dice = 4
 game_id = 0
 sides_dice = 6 #required
-roll_mod = 0
+roll_mod = None
 kd_mod = "d"
 kd_num = 1
 rl_mod = "r"
@@ -14,38 +14,47 @@ rl_num = 8
 
 roll_dict = {"iterations" : 6}
 roll_print = []
-if(not iterations):
-    iterations=1
+if(iterations is None):
+    iterations = 1
 for x in range(iterations):
     die_roll = []
-    for x in range(num_dice):
+    kd_print, sum_print, rl_print = '', '', ''
+    for y in range(num_dice):
         die_roll.append(random.randint(1, sides_dice))
     
     if(kd_mod):
         dice_dropped = []
         dice_kept = []
         die_roll.sort()
-        if ( not kd_num):
+        if(kd_num is None):
             kd_num = 1
-        if kd_mod == "k" :
+        if(kd_mod == "k"):
             dice_dropped = die_roll[(num_dice-kd_num):num_dice]
             dice_kept = die_roll[0:(num_dice-kd_num)]
-        elif kd_mod == "d" :
+        elif(kd_mod == "d"):
             dice_dropped = die_roll[0:kd_num]
             dice_kept = die_roll[kd_num:num_dice]
+        kd_print = " Dice Kept: " + str(dice_kept) + " Dice Dropped: " + str(dice_dropped)
             
     sum = 0
-    for x in dice_kept:
-        sum += x
+    for i in dice_kept:
+        sum += i
+    if(roll_mod):
+        sum += roll_mod
     if(rl_mod and rl_num):
         if(rl_mod == "r"):
             if(sum < rl_num):
                 sum = rl_num
+                rl_print = " Raised to " + str(rl_num)
         elif(rl_mod == "l"):
             if(sum > rl_num):
                 sum = rl_num
-    roll_print.append("Result: " + str(sum) + " Dice Kept: " + str(dice_kept) + " Dice Dropped: " + str(dice_dropped))
+                rl_print = " Lowered to " + str(rl_num)
+    sum_print = "Result: " + str(sum)
+    if(roll_mod):
+        sum_print = "Result: " +str(sum)+ "  (Modified with " + str(roll_mod) + ")"            
+    roll_print.append(sum_print + rl_print + kd_print)
+    
     
 for x in roll_print:
     print(x)
-
