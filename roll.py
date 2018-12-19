65dc53383b1d7bd347cf03db054497b00eca612d
MaxiStaples <1977518+MaxiStaples@users.noreply.github.com>
2018-12-18 17:29:11 -0800

    Update roll.py
    -Added default roll parameters
    -Put roll parameters into a dictionary
    -kd_mod, kd_num and rl_mod, rl_num consolidated to tuples

--- roll.py
+++ roll.py
@@ -1,58 +1,71 @@
 import random
 
-#roll_in = [iterations, num_dice, game_id, sides_dice, roll_mod, kd_mod, kd_num, rl_mod, rl_num]
+roll_in = {         # Dictionary defines roll from parameters passed from parser
+'iterations' : 6,   # how many times the rolls happen
+'num_dice' : 4,     # number of dice per roll
+'game_id' : 'd',      # Game identifier (not implemented)
+'sides_dice' : 6,   # (required) Number of sides on the die
+'roll_mod' : 0,  # Any +/- modifier to the roll
+'kd_mod' : ( 'd' , 1 ),     # (k)eep or (d)rop high or low; number of dice to keep/drop (can be negative)
+'rl_mod' : ( 'r' , 8 ),     # (r)aise or (l)ower total roll if it goes beyond threshol;, threshold value
+}
 
-iterations = 5   # how many times the rolls happen
-num_dice = 4     # number of dice per roll
-game_id = 0      # Game identifier (not implemented)
-sides_dice = 6   # (required) Number of sides on the die
-roll_mod = None  # Any +/- modifier to the roll
-kd_mod = "d"     # (k)eep or (d)rop high or low
-kd_num = 1       # The number of dice to keep or drop (can be negative) 
-rl_mod = "r"     # (r)aise or (l)ower total roll if it goes beyond threshold
-rl_num = 8       # The raise/lower threshold
+# The default roll will populate any parameters left blank in the parser
+default_roll = {    
+'iterations' : 1,   
+'num_dice' : 1,     
+'game_id' : 'd',      
+'sides_dice' : 20, 
+'roll_mod' : 0,  
+'kd_mod' : (None, None),    
+'rl_mod' : (None, None)
+}          
 
-roll_dict = {"iterations" : 6}
+for key in roll_in:
+    if not roll_in[key]:
+        roll_in[key] = default_roll[key]
+
+
 roll_print = []
-if(iterations is None):
-    iterations = 1
-for x in range(iterations):
+
+for x in range(roll_in['iterations']):
     die_roll = []
     kd_print, sum_print, rl_print = '', '', ''
-    for y in range(num_dice):
-        die_roll.append(random.randint(1, sides_dice))
+    for y in range(roll_in['num_dice']):
+        die_roll.append(random.randint(1, roll_in['sides_dice']))
     
-    if(kd_mod):
+    if(roll_in['kd_mod'][0]):
         dice_dropped = []
         dice_kept = []
         die_roll.sort()
-        if(kd_num is None):
-            kd_num = 1
-        if(kd_mod == "k" and kd_num > 0):
-            dice_dropped = die_roll[(num_dice-kd_num):num_dice]
-            dice_kept = die_roll[0:(num_dice-kd_num)]
-        elif(kd_mod == "d"):
-            dice_dropped = die_roll[0:kd_num]
-            dice_kept = die_roll[kd_num:num_dice]
+        if(roll_in['kd_mod'][1] is None):
+            roll_in['kd_mod'][1] = 1
+        if(roll_in['kd_mod'][0] == "k" and roll_in['kd_mod'][1] > 0):
+            dice_dropped = die_roll[(roll_in['num_dice']-roll_in['kd_mod'][1]):roll_in['num_dice']]
+            dice_kept = die_roll[0:(roll_in['num_dice']-roll_in['kd_mod'][1])]
+        elif(roll_in['kd_mod'][0] == "d"):
+            dice_dropped = die_roll[0:roll_in['kd_mod'][1]]
+            dice_kept = die_roll[roll_in['kd_mod'][1]:roll_in['num_dice']]
         kd_print = " Dice Kept: " + str(dice_kept) + " Dice Dropped: " + str(dice_dropped)
+    else:
+        dice_kept = die_roll
             
     sum = 0
     for i in dice_kept:
-        sum += i
-    if(roll_mod):
-        sum += roll_mod
-    if(rl_mod and rl_num):
-        if(rl_mod == "r"):
-            if(sum < rl_num):
-                sum = rl_num
-                rl_print = " Raised to " + str(rl_num)
-        elif(rl_mod == "l"):
-            if(sum > rl_num):
-                sum = rl_num
-                rl_print = " Lowered to " + str(rl_num)
+        sum += i 
+        sum += roll_in['roll_mod']
+    if(roll_in['rl_mod'][0] and roll_in['rl_mod'][1]):
+        if(roll_in['rl_mod'][0] == "r"):
+            if(sum < roll_in['rl_mod'][1]):
+                sum = roll_in['rl_mod'][1]
+                rl_print = " Raised to " + str(roll_in['rl_mod'][1])
+        elif(roll_in['rl_mod'][0] == "l"):
+            if(sum > roll_in['rl_mod'][1]):
+                sum = roll_in['rl_mod'][1]
+                rl_print = " Lowered to " + str(roll_in['rl_mod'][1])
     sum_print = "Result: " + str(sum)
-    if(roll_mod):
-        sum_print = "Result: " +str(sum)+ "  (Modified with " + str(roll_mod) + ")"            
+    if(roll_in['roll_mod']):
+        sum_print = "Result: " +str(sum)+ "  (Modified with " + str(roll_in['roll_mod']) + ")"            
     roll_print.append(sum_print + rl_print + kd_print)
     
     
